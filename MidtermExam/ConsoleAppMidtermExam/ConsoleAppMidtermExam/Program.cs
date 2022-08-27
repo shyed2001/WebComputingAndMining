8aede89d1f5a808b360d67acbd49c1f707ce5456
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2022-08-27 19:14:25 +0600

    ok
    ok

---
+++ MidtermExam/ConsoleAppMidtermExam/ConsoleAppMidtermExam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace MidExam_Answer
+{
+    public delegate string DlReplaceSpace(string str);
+    public class Program
+    {
+        /*
+         public static string ReplaceSpaces(string s)
+         {
+             Console.WriteLine("Replacing spaces with hyphens.");
+             return s.Replace(' ', '-');
+         }
+        */
+    }
+    public class Test
+    {
+        public static void Main(String[] args)
+        {
+            string s = "Hello  ";
+
+            DlReplaceSpace dlrs = (string str) =>
+            {
+                Console.WriteLine("Replacing spaces with hyphens.");
+                return s.Replace(' ', '-');
+            };
+
+            Console.WriteLine(dlrs(s));
+            // Console.WriteLine(Program.ReplaceSpaces(s));
+        }
+    }
+}
